effe37980665469751c5a4f08a460a5545121aa0
s-bilic <24295554+s-bilic@users.noreply.github.com>
2023-09-26 01:07:57 +0200

    Added getTokenPriceHistory function for fetching previous specifc token prices

--- lib/api.ts
+++ lib/api.ts
@@ -26,13 +26,42 @@ const getTokenPrices = async (client: any, acceptedTokens: any) => {
   return data;
 };
 
-const getTokenPricesHistory = async (id: any, timestamp: any) => {
-  const url = `https://benchmarks.pyth.network/v1/updates/price/${timestamp}?ids=${id}&parsed=true
-  `;
+// const timestamp = Math.floor(Date.now() / 1000) - 10;
+const getTokenPriceHistory = async (
+  id: any,
+  timestamp: string,
+  onlyPrice: boolean,
+) => {
+  const date = new Date();
+  let targetTimestamp;
+
+  if (timestamp === "prevDay") {
+    targetTimestamp = Math.floor(date.setDate(date.getDate() - 1) / 1000);
+  } else if (timestamp === "prevWeek") {
+    targetTimestamp = Math.floor(date.setDate(date.getDate() - 7) / 1000);
+  } else if (timestamp === "prevMonth") {
+    targetTimestamp = Math.floor(date.setMonth(date.getMonth() - 1) / 1000);
+  } else if (!isNaN(timestamp)) {
+    // If a numeric timestamp is provided, use it as is
+    targetTimestamp = parseInt(timestamp, 10);
+  } else {
+    throw new Error("Invalid timestamp value");
+  }
+
+  const url = `https://benchmarks.pyth.network/v1/updates/price/${targetTimestamp}?ids=${id}&parsed=true`;
+
   const response = await fetch(url);
   const data = await response.json();
 
-  return data;
+  if (onlyPrice) {
+    const price = data?.parsed[0]?.price?.price;
+    const expo = data?.parsed[0]?.price?.expo;
+    const formattedPrice = Number(price) * Math.pow(10, expo);
+
+    return formattedPrice;
+  } else {
+    return data;
+  }
 };
 
-export { getTokenList, getTransactions, getTokenPrices, getTokenPricesHistory };
+export { getTokenList, getTransactions, getTokenPrices, getTokenPriceHistory };
